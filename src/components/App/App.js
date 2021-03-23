c8d109833be30e026f647d3feefd26a2f91cca03
Cooper Saye <coopersaye@gmail.com>
2021-03-23 07:30:59 -0600

    Initialize firebase.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,25 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import { firebaseConfig } from '../../util/firebaseConfig.js';
+import { useAuthState } from 'react-firebase-hooks/auth';
+
 import Time from '../Time/Time.js';
+import CoinFlip from '../CoinFlip/CoinFlip.js';
+import DiceRoll from '../DiceRoll/DiceRoll.js';
 
 import './App.css';
 
+firebase.initializeApp(firebaseConfig);
+
 function App() {
+  useAuthState(firebase.auth());
+
   return (
     <div className="App">
       <Time />
+      <CoinFlip />
+      <DiceRoll />
     </div>
   );
 }
