a6b4fdcb71e5f1b6e7550dea43b32f0babfb7cac
Cooper Saye <coopersaye@gmail.com>
2021-03-24 11:43:55 -0600

    Add DistConvert units.

--- src/components/DistConvert/DistConvert.js
+++ src/components/DistConvert/DistConvert.js
@@ -2,6 +2,12 @@ import React, { useState } from 'react';
 
 import './DistConvert.css';
 
+const kmToM = 1000;
+const cmToM = 0.01;
+const miToM = 1609.34;
+const fToM = 0.3048;
+const inToM = 0.0254;
+
 function DistConvert() {
   const [inDist, setInDist] = useState("0");
   const [outDist, setOutDist] = useState(undefined);
@@ -12,12 +18,21 @@ function DistConvert() {
     e.preventDefault();
     const distIn = parseFloat(inDist);
     let factor = 1;
-    // get conversion factor
-    if (inUnit == "m" && outUnit == "f") factor = 3.281;
-    else if (inUnit == "f" && outUnit == "m") factor = 0.305;
+    // convert to meters
+    if (inUnit === "km") factor *= kmToM;
+    else if (inUnit === "cm") factor *= cmToM;
+    else if (inUnit === "mi") factor *= miToM;
+    else if (inUnit === "f") factor *= fToM;
+    else if (inUnit === "in") factor *= inToM;
+    // convert to out unit
+    if (outUnit === "km") factor *= (1 / kmToM);
+    else if (outUnit === "cm") factor *= (1 / cmToM);
+    else if (outUnit === "mi") factor *= (1 / miToM);
+    else if (outUnit === "f") factor *= (1 / fToM);
+    else if (outUnit === "in") factor *= (1 / inToM);
     let distOut = distIn * factor;
-    // round to two decimal places
-    distOut = Math.round(distOut * 100) / 100;
+    // round to three decimal places
+    distOut = Math.round(distOut * 1000) / 1000;
     setOutDist(distOut + outUnit);
   }
 
@@ -26,16 +41,24 @@ function DistConvert() {
       <h1>Dist Convert</h1>
       <form onSubmit={convertDist}>
         <label htmlFor="distconvert-indist">Distance</label>
-        <input id="baseconvert-indist" value={inDist} type="number" onChange={e => setInDist(e.target.value)} required />
+        <input id="baseconvert-indist" value={inDist} type="number" step="any" onChange={e => setInDist(e.target.value)} required />
         <label htmlFor="distconvert-inunit">In Unit</label>
         <select id="distconvert-inunit" value={inUnit} onChange={e => setInUnit(e.target.value)}>
+          <option value="km">Kilometers</option>
           <option value="m">Meters</option>
+          <option value="cm">Centimeters</option>
+          <option value="mi">Miles</option>
           <option value="f">Feet</option>
+          <option value="in">Inches</option>
         </select>
         <label htmlFor="distconvert-outunit">Out Unit</label>
         <select id="distconvert-outunit" value={outUnit} onChange={e => setOutUnit(e.target.value)}>
+        <option value="km">Kilometers</option>
         <option value="m">Meters</option>
+        <option value="cm">Centimeters</option>
+        <option value="mi">Miles</option>
         <option value="f">Feet</option>
+        <option value="in">Inches</option>
         </select>
         <button type="submit">Convert</button>
       </form>
