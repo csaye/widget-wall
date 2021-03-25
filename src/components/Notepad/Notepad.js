2c3802fbb2e22ceb815790c5a6b6a992e08963e4
Cooper Saye <coopersaye@gmail.com>
2021-03-24 18:54:11 -0600

    Update Notepad component.

--- src/components/Notepad/Notepad.js
+++ src/components/Notepad/Notepad.js
@@ -9,22 +9,29 @@ function Notepad() {
   const [saved, setSaved] = useState(false);
 
   const uid = firebase.auth().currentUser.uid;
+  const notepadRef = firebase.firestore().collection('notepad').doc(uid);
 
   async function getNote() {
-    const noteRef = firebase.firestore().collection('notes').doc(uid);
-    noteRef.get().then(doc => {
-      if (doc.exists) setNote(doc.data().note);
-    })
+    notepadRef.get().then(doc => {
+      if (doc.exists) {
+        const docData = doc.data();
+        setNote(docData.note);
+      }
+    });
   }
 
   useEffect(() => {
+    // get note on start
     getNote();
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-  function saveNote() {
-    firebase.firestore().collection('notes').doc(uid).set({
-      note
+  function saveNote(e) {
+    e.preventDefault();
+    // update firebase
+    notepadRef.set({
+      note: note
     });
+    // show saved message with timeout
     setSaved(true);
     setTimeout(() => {
       setSaved(false)
@@ -34,8 +41,10 @@ function Notepad() {
   return (
     <div className="Notepad widget">
       <h1>Notepad</h1>
-      <textarea rows="6" value={note} onChange={e => setNote(e.target.value)} />
-      <button onClick={saveNote}>Save Note</button>
+      <form onSubmit={saveNote}>
+        <textarea rows="6" value={note} onChange={e => setNote(e.target.value)} />
+        <button type="submit">Save Note</button>
+      </form>
       {saved && <p className="success">Note saved</p>}
     </div>
   );
