582237a0124b4cb70586016dc3ed8733056e613a
Cooper Saye <coopersaye@gmail.com>
2021-03-29 19:46:29 -0600

    Improve widget layout.

--- src/components/TextCount/TextCount.js
+++ src/components/TextCount/TextCount.js
@@ -10,7 +10,6 @@ function TextCount() {
       <h1>Text Count</h1>
       <textarea rows="4" value={text} onChange={e => setText(e.target.value)} />
       <p>Word count: {!text ? 0 : text.trim().split(' ').length}</p>
-      {console.log(text.trim().split(' '))}
       <p>Character count: {text.length}</p>
     </div>
   )
