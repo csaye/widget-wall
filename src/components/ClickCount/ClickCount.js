a3a2e3ed8dfa47c61c571ae31c24947beaf8ce38
Cooper Saye <coopersaye@gmail.com>
2021-03-29 19:56:28 -0600

    Improve ClickCount component.

--- src/components/ClickCount/ClickCount.js
+++ src/components/ClickCount/ClickCount.js
@@ -5,9 +5,13 @@ import firebase from 'firebase/app';
 import './ClickCount.css';
 
 let totalClicks = 0;
+let bestCps = 0
+let cps = 0;
 
 function ClickCount() {
   const [totalDisplay, setTotalDisplay] = useState(0);
+  const [cpsDisplay, setCpsDisplay] = useState(0);
+  const [bestCpsDisplay, setBestCpsDisplay] = useState(0);
 
   const uid = firebase.auth().currentUser.uid;
   const clicksRef = firebase.firestore().collection('clicks').doc(uid);
@@ -18,21 +22,37 @@ function ClickCount() {
       if (doc.exists) {
         const docData = doc.data();
         totalClicks = docData.totalClicks;
+        bestCps = docData.bestCps;
         updateDisplay();
       }
     });
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
+  useEffect(() => {
+    const cpsInterval = setInterval(() => {
+      if (cps > 0) {
+        if (cps > bestCps) bestCps = cps;
+        cps = 0;
+        updateDisplay();
+      }
+    }, 1000);
+    return () => clearInterval(cpsInterval);
+  }, []);
+
   function updateDisplay() {
     setTotalDisplay(totalClicks);
+    setCpsDisplay(cps);
+    setBestCpsDisplay(bestCps);
   }
 
-  function incrementClick() {
+  async function incrementClick() {
     totalClicks += 1;
+    cps += 1;
     updateDisplay();
     // update firebase
-    clicksRef.set({
-      totalClicks
+    await clicksRef.set({
+      totalClicks,
+      bestCps
     });
   }
 
@@ -40,7 +60,9 @@ function ClickCount() {
     <div className="ClickCount widget">
       <h1>Click Count</h1>
       <button onClick={incrementClick}>Click</button>
+      <p>{cpsDisplay}</p>
       <p>Total clicks: {totalDisplay}</p>
+      <p>Best clicks per second: {bestCpsDisplay}</p>
     </div>
   );
 }
