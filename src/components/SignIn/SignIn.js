7d4aae26b09ce8f0e14a5050839977fdff334b34
Cooper Saye <coopersaye@gmail.com>
2021-04-03 17:06:50 -0600

    Improve SignIn component styling.

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,5 +1,8 @@
 import firebase from 'firebase/app';
 
+import BackgroundImage from '../BackgroundImage/BackgroundImage.js';
+
+import defaultBackground from '../../img/default.jpg';
 import './SignIn.css';
 
 function SignIn() {
@@ -9,10 +12,13 @@ function SignIn() {
   }
 
   return (
-    <div className="SignIn widget">
-      <h1>Widget Wall</h1>
-      <button onClick={signInWithGoogle}>Sign in with Google</button>
-    </div>
+    <>
+      <div className="SignIn widget">
+        <h1>Widget Wall</h1>
+        <button onClick={signInWithGoogle}>Sign in with Google</button>
+      </div>
+      <BackgroundImage src={defaultBackground} />
+    </>
   );
 }
 
